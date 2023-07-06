36205a1eb73a1ce2f06bc0d03ac92907f6eefe9c
Walter Liendo <walter.liendo@gmail.com>
2023-07-05 21:55:04 -0300

    agregue los servicios

--- controllers/login.js
+++ controllers/login.js
@@ -1,20 +1,54 @@
+var emailInput = document.getElementById("email");
+var passwordInput = document.getElementById("password");
+
+emailInput.addEventListener("input", function() {
+    if (isEmailValid(emailInput.value)) {
+        emailInput.classList.remove("error");
+        emailInput.classList.add("success");
+    } else {
+        emailInput.classList.remove("success");
+    }
+});
+
+passwordInput.addEventListener("input", function() {
+    if (passwordInput.value.length >= 5) {
+        passwordInput.classList.remove("error");
+        passwordInput.classList.add("success");
+    } else {
+        passwordInput.classList.remove("success");
+    }
+});
+
 document.getElementById("login__form").addEventListener("submit", function(event) {
     event.preventDefault();
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
 
-    if (!isEmailValid(email)) {
+    if (!isEmailValid(emailInput.value)) {
+        emailInput.classList.add("error");
+        passwordInput.classList.remove("success");
         alert("Por favor, ingrese un correo electrónico válido.");
         return;
     }
 
-    if (password.length < 5) {
+    if (passwordInput.value.length < 5) {
+        passwordInput.classList.add("error");
+        emailInput.classList.remove("success");
         alert("La contraseña debe tener al menos 5 caracteres.");
         return;
     }
-    document.getElementById("login__form").reset();
-    window.location.href = "../screens/producto.html";
 
+    // Limpio el formulario:
+    document.getElementById("login__form").reset();
+
+    // Redirecciono al html de Producto:
+    window.location.href="product.html";
+
+    // Quita los estilos de error:
+    emailInput.classList.remove("error");
+    passwordInput.classList.remove("error");
+
+    // Agrega el estilo de éxito
+    emailInput.classList.add("success");
+    passwordInput.classList.add("success");
 });
 
 function isEmailValid(email) {
--- services/product-service.js
+++ services/product-service.js
@@ -1,9 +1,71 @@
-// METHOD GET 
+// METHOD GET
 const AllProducts = () =>
-    fetch("http://localhost:3000/producto")
+  fetch("http://localhost:3000/producto")
     .then((response) => response.json())
     .catch((error) => console.log(error));
 
+const getOneProduct = (id) => {
+  return fetch(`http://localhost:3000/producto/${id}`).then((response) => {
+    return response.json();
+  });
+};
+
+//POST
+const addProduct = (name, imageUrl, price, category, description) => {
+  return fetch(`http://localhost:3000/producto`, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      imageUrl,
+      price,
+      category,
+      description,
+    }),
+  }).then((response) => {
+    if (response.ok) {
+      return response.body;
+    }
+    throw new Error("No se pudo crear el nuevo producto");
+  });
+};
+
+// PUT / PATCH
+const modifyProduct = async (id, name, price, category, description) => {
+  return fetch(`http://localhost:3000/producto/${id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      price,
+      category,
+      description,
+    }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((error) => console.log(error));
+};
+
+//DELETE
+const deleteProduct = async (id) => {
+  return await fetch(`http://localhost:3000/producto/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
 export const productServices = {
-AllProducts
+  AllProducts,
+  getOneProduct,
+  addProduct,
+  modifyProduct,
+  deleteProduct,
 };
