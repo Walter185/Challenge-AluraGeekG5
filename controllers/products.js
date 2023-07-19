ba4a8127072c59621bfc7a4865fb49b76b45476d
Walter Liendo <walter.liendo@gmail.com>
2023-07-19 14:31:27 -0300

    correcion de rutas

--- controllers/products.js
+++ controllers/products.js
@@ -13,7 +13,7 @@ const nuevoProducto = (name, price, imageUrl, id) => {
           </a>
           <h1 class="producto__name"> ${name} </h1>
           <p class="producto__price">USD ${price}</p>
-          <a class="producto__ver" href="./one.html?id=${id}">
+          <a class="producto__ver" href="./pages/one.html?id=${id}">
           Ver Producto</a>
         </div>   
     `;
